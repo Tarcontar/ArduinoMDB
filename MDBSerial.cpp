2433e4361a09b4f07598256616649c7cd0494eb8
Tarcontar <michael-schnabel@gmx.net>
2018-03-08 19:23:21 +0100

    use UART lib for communication

--- MDBSerial.cpp
+++ MDBSerial.cpp
@@ -1,176 +1,40 @@
 #include "MDBSerial.hpp"
-#include <Arduino.h>
-#include <avr/interrupt.h>
+#include "UART.hpp"
 
-struct data
+UART *m_uart;
+
+MDBSerial::MDBSerial(uint8_t uart) 
 {
-	char value;
-	char mode;
-};
-
-volatile data v_buffer[100];
-volatile int v_pos = 0;
-volatile bool v_error = false;
-volatile bool v_msg_complete = false;
-volatile int v_uart = 0;
-
-volatile unsigned char *m_UDRn;
-volatile unsigned char *m_UBRRnH;
-volatile unsigned char *m_UBRRnL;
-volatile unsigned char *m_UCSRnA;
-volatile unsigned char *m_UCSRnB;
-volatile unsigned char *m_UCSRnC;
-
-MDBSerial::MDBSerial(int uart) 
-{
-	v_uart = uart;
-	if (uart == 1)
-	{
-		m_TXn = 18;
-		m_UDRn = &UDR1;
-		m_RXENn = RXEN1;
-		m_TXENn = TXEN1;
-		m_UBRRnH = &UBRR1H;
-		m_UBRRnL = &UBRR1L;
-		m_UCSRnA = &UCSR1A;
-		m_UCSRnB = &UCSR1B;
-		m_UCSRnC = &UCSR1C;
-		m_UCSZn0 = UCSZ10;
-		m_UCSZn1 = UCSZ11;
-		m_UCSZn2 = UCSZ12;
-	}
-	else if (uart == 2)
-	{
-		m_TXn = 16;
-		m_UDRn = &UDR2;
-		m_RXENn = RXEN2;
-		m_TXENn = TXEN2;
-		m_UBRRnH = &UBRR2H;
-		m_UBRRnL = &UBRR2L;
-		m_UCSRnA = &UCSR2A;
-		m_UCSRnB = &UCSR2B;
-		m_UCSRnC = &UCSR2C;
-		m_UCSZn0 = UCSZ20;
-		m_UCSZn1 = UCSZ21;
-		m_UCSZn2 = UCSZ22;
-	}
-	else if (uart == 3)
-	{
-		m_TXn = 14;
-		m_UDRn = &UDR3;
-		m_RXENn = RXEN3;
-		m_TXENn = TXEN3;
-		m_UBRRnH = &UBRR3H;
-		m_UBRRnL = &UBRR3L;
-		m_UCSRnA = &UCSR3A;
-		m_UCSRnB = &UCSR3B;
-		m_UCSRnC = &UCSR3C;
-		m_UCSZn0 = UCSZ30;
-		m_UCSZn1 = UCSZ31;
-		m_UCSZn2 = UCSZ32;
-	}
-	else
-	{
-		m_TXn = 1;
-		m_UDRn = &UDR0;
-		m_RXENn = RXEN0;
-		m_TXENn = TXEN0;
-		m_UBRRnH = &UBRR0H;
-		m_UBRRnL = &UBRR0L;
-		m_UCSRnA = &UCSR0A;
-		m_UCSRnB = &UCSR0B;
-		m_UCSRnC = &UCSR0C;
-		m_UCSZn0 = UCSZ00;
-		m_UCSZn1 = UCSZ01;
-		m_UCSZn2 = UCSZ02;
-	}
+	m_uart = UART::UART9Bit(uart);
 	//hardReset(); //does not work at the moment
 	init();
 }
 
-ISR(USART1_RX_vect)
-{
-	if (v_uart == 1)
-	{
-		char status = *m_UCSRnA;
-
-		if (status & ((1 << FE) | (1 << DOR) | (1 << UPE)))
-		{
-			v_error = true;
-		}
-		v_buffer[v_pos].mode = (*m_UCSRnB >> 1) & 0x01;
-		if (v_buffer[v_pos].mode == 1)
-		{
-			//we got an ack or an checksum
-			v_msg_complete = true;
-		}
-		v_buffer[v_pos++].value = *m_UDRn;
-	}
-}
-
-ISR(USART2_RX_vect)
-{
-	if (v_uart == 2)
-	{
-		char status = *m_UCSRnA;
-
-		if (status & ((1 << FE) | (1 << DOR) | (1 << UPE)))
-		{
-			v_error = true;
-		}
-		v_buffer[v_pos].mode = (*m_UCSRnB >> 1) & 0x01;
-		if (v_buffer[v_pos].mode == 1)
-		{
-			//we got an ack or an checksum
-			v_msg_complete = true;
-		}
-		v_buffer[v_pos++].value = *m_UDRn;
-	}
-}
-
-ISR(USART3_RX_vect)
-{
-	if (v_uart == 3)
-	{
-		char status = *m_UCSRnA;
-
-		if (status & ((1 << FE) | (1 << DOR) | (1 << UPE)))
-		{
-			v_error = true;
-		}
-		v_buffer[v_pos].mode = (*m_UCSRnB >> 1) & 0x01;
-		if (v_buffer[v_pos].mode == 1)
-		{
-			//we got an ack or an checksum
-			v_msg_complete = true;
-		}
-		v_buffer[v_pos++].value = *m_UDRn;
-	}
-}
-
 void MDBSerial::hardReset()
 {
+	/*
 	pinMode(m_TXn, OUTPUT);
 	digitalWrite(m_TXn, HIGH);
 	delay(TIME_SETUP);
 	digitalWrite(m_TXn, LOW);
 	delay(TIME_BREAK);
 	digitalWrite(m_TXn, HIGH);
+	*/
 }
 
 void MDBSerial::Ack()
 {
-	write(ACK, DATA);
+	m_uart->write9bit(ACK);
 }
 
 void MDBSerial::Nak()
 {
-	write(NAK, DATA);
+	m_uart->write9bit(NAK);
 }
 
 void MDBSerial::Ret()
 {
-	write(RET, DATA);
+	m_uart->write9bit(RET);
 }
 
 void MDBSerial::SendCommand(int address, int cmd, int *data, int dataCount)
@@ -181,23 +45,21 @@ void MDBSerial::SendCommand(int address, int cmd, int *data, int dataCount)
 void MDBSerial::SendCommand(int address, int cmd,  int subCmd, int *data, int dataCount)
 {
 	char sum = 0;
-	v_msg_complete = false;
-	
-	write(address | cmd, ADDRESS);
+	m_uart->write9bit(address | cmd | 0x100);
 	sum += address | cmd;
 	
 	if (subCmd >= 0)
 	{
-		write(subCmd, DATA);
+		m_uart->write9bit(subCmd);
 		sum += subCmd;
 	}
 
 	for (int i = 0; i < dataCount; i++)
 	{
-		write(data[i], DATA);
+		m_uart->write9bit(data[i]);
 		sum += data[i];
 	}
-	write(sum, DATA); //send the checksum
+	m_uart->write9bit(sum); //send the checksum
 	delay(RESPONSE_TIME * 2);
 }
 
@@ -210,15 +72,13 @@ int MDBSerial::GetResponse(char data[], int *count, int num_bytes)
 	delay(RESPONSE_TIME);
 	for (int i = 0; i < num_bytes; i++)
 	{
-		if (v_msg_complete)
+		if (m_uart->ninthBitSet())
 		{
-			v_msg_complete = false;
 			break;
 		}
-		else if (v_error)
+		else if (m_uart->error())
 		{
-			v_error = false;
-			v_pos = 0;
+			m_uart->flush();
 			return -1;
 		}
 		else
@@ -228,23 +88,17 @@ int MDBSerial::GetResponse(char data[], int *count, int num_bytes)
 	}
 	
 	//nothing received
-	if (v_pos == 0)
+	if (!m_uart->available())
 		return -2;
  
-    *count = v_pos - 1; //do not send checksum
-	for (int i = 0; i < v_pos; i++)
+	while (m_uart->available())
 	{
-		char val = v_buffer[i].value;
-		if (v_buffer[i].mode != 1)
+		int val = m_uart->read();
+		if (val & 0x100)
 		{
-			data[i] = val;
-			sum += val;
-		}
-		else
-		{
-			if (v_pos == 1) //we got an ACK //or NAK or RET??
+			if (*count == 1) //we got an ACK //or NAK or RET??
 			{
-				v_pos = 0;
+				m_uart->flush();
 				if (val == ACK)
 					return ACK;
 				else if (val == NAK)
@@ -254,35 +108,17 @@ int MDBSerial::GetResponse(char data[], int *count, int num_bytes)
 			{
 				if (sum != val)
 				{
-					v_pos = 0;
+					m_uart->flush();
 					return -3;
 				}
 			}
 		}
+		else
+		{
+			data[(*count)++] = val;
+			sum += val;
+		}
 	}
-	v_pos = 0;
+	m_uart->flush();
 	return 1;
-}
-
-void MDBSerial::init()
-{
-	*m_UCSRnB = (1 << RXEN1) | (1 << TXEN1) | (1 << RXCIE1);
-	*m_UBRRnH = 0;
-	*m_UBRRnL = 103; // 9600baud
-	*m_UCSRnA &= ~(1 << U2X1); //disable rate doubler //should be U2Xn ??
-	*m_UCSRnB |= (1 << UCSZ12); //9 bit mode
-	*m_UCSRnC |= (1 << UCSZ11) | (1 << UCSZ10); //also 9bit mode
-	*m_UCSRnC |= 0b00000100; //one stop bit
-	*m_UCSRnC |= 0b00000000; //no parity bit
-}
-
-void MDBSerial::write(int cmd, int mode)
-{
-	while (!(*m_UCSRnA & (1 << UDRE))) {}
-	if (mode)
-		*m_UCSRnB |= (1 << TXB8);
-	else
-		*m_UCSRnB &= ~(1 << TXB8);
-	*m_UDRn = cmd;
-	m_commandSentTime = millis();
 }
--- MDBSerial.hpp
+++ MDBSerial.hpp
@@ -1,34 +1,27 @@
 #pragma once
+#include <Arduino.h>
 
 #define MDB_DEBUG TRUE//undefine to minimize code
-
-//define registers
-#define TXB8 	0
-#define UPE 	2
-#define DOR 	3
-#define FE 		4
-#define UDRE 	5
-#define RXC 	7
 
 //MDB specific stuff
 #define DATA_MAX 36
 
-#define TIME_OUT 		5
+#define TIME_OUT 	5
 #define TIME_BREAK 	100
 #define TIME_SETUP 	200
 
-#define ACK 	0x00
-#define RET 	0xAA
-#define NAK 	0xFF
+#define ACK 	0x100
+#define RET 	0x1AA
+#define NAK 	0x1FF
 
 #define INTER_BYTE_TIME 	1
 #define RESPONSE_TIME 		5
-#define BYTE_SEND_TIME 	1.2
+#define BYTE_SEND_TIME 		1.2
 
 class MDBSerial
 {
 public:
-	MDBSerial(int uart = 0);
+	MDBSerial(uint8_t uart = 0);
 	
 	void Ack();
 	void Nak();
@@ -39,26 +32,7 @@ public:
 	int GetResponse(char data[] = 0, int *count = 0, int num_bytes = 0);
 
 private:
-
-	enum MODE {
-		DATA = 0,
-		ADDRESS = 1
-	};
-
 	void hardReset();
-	void init();
-	void write(int cmd, int mode);
-
-private:
-	unsigned long m_commandSentTime;
-
-	int m_TXn;
-
-	volatile unsigned char  m_RXENn;
-	volatile unsigned char  m_TXENn;
-	volatile unsigned char  m_UCSZn0;
-	volatile unsigned char  m_UCSZn1;
-	volatile unsigned char  m_UCSZn2;
 };
 
 
